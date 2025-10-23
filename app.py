2e793da079d35bb1630bf220f2c5b77476ea48c5
SwordForShinobi <95093203+SwordForShinobi@users.noreply.github.com>
2025-10-23 14:26:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+pip install scikit-learn
+
 import streamlit as st
 import pandas as pd
 import pickle
@@ -58,5 +60,6 @@ if st.button("Предсказать отток"):
         st.error(f"🆘Высокий риск оттока (вероятность оттока: {probability[0][1]: .2%)}")
     else:
         st.success(f"🎉Низкий риск оттока (вероятность оттока: {probability[0][0]: .2%}")
+
 
 
